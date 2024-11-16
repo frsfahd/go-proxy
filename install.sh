6f110fc8f51ad2a31f5f9d45bf5bea49b37f1397
farraselfahd <farraselfahd2000@gmail.com>
2024-11-16 07:35:26 +0700

    add install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Fetch the latest release tag from GitHub API
+LATEST_TAG=$(curl -s https://api.github.com/repos/frsfahd/go-proxy/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+
+# Construct the URL for the latest release binary
+BINARY_URL="https://github.com/frsfahd/go-proxy/releases/download/$LATEST_TAG/go-proxy"
+
+# Download the binary from GitHub
+curl -L -o /usr/local/bin/go-proxy $BINARY_URL
+
+# Make the binary executable
+chmod +x /usr/local/bin/go-proxy
+
+echo "Installation complete!"
